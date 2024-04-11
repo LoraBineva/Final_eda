bcc7ab3faa71708351d6be3ad70062ce2fee3a4f
LoraBineva <124515927+LoraBineva@users.noreply.github.com>
2024-04-11 12:36:10 +0300

    Update Log_In.cs

--- Log_In.cs
+++ Log_In.cs
@@ -1,20 +1,59 @@
+using Register_page;
+using System.Data;
+using System.Data.SqlClient;
+using static Final_Eda.Authenticate;
+
 namespace Final_Eda
 {
     public partial class Log_In : Form
     {
+        SqlConnection sql;
         public Log_In()
         {
             InitializeComponent();
+            try
+            {
+                sql = new SqlConnection(@"Data Source=LAB109PC01\SQLEXPRESS; Initial Catalog=GoodWatch; Integrated Security=True;");
+                sql.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
-        private void Log_In_Load(object sender, EventArgs e)
+        private void log_in_btn_Click(object sender, EventArgs e)
         {
+            string username_inp = username.Text;
+            string password_inp = password.Text;
 
+            if (!string.IsNullOrWhiteSpace(username_inp) && !string.IsNullOrWhiteSpace(password_inp))
+            {
+                Authenticate.User authenticatedUser = DatabaseManager.AuthenticateUser(username_inp, password_inp);
+
+                if (authenticatedUser != null)
+                {
+                    string insertUsername = "Insert Into Booking Values (@username, NULL)";
+                    SqlCommand cmd = new SqlCommand(insertUsername, sql);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@username", authenticatedUser.Username);
+                    cmd.ExecuteNonQuery();
+                    this.Hide();
+                   //   Search search = new Search(authenticatedUser);
+                   // search.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter both username and password.");
+            }
         }
 
-        private void login_btn_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
         {
-
+            this.Hide();
+            Register register = new Register();
+            register.ShowDialog();
         }
     }
 }
